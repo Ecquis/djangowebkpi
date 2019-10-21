c4e90ebe72b402ad0ac814b20acbacd7724b2c29
TastyCake <gotastycake@gmail.com>
2019-10-21 03:07:10 +0300

    fixes

--- webkpi/views.py
+++ webkpi/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import Position, Department, Employee
+from django.core.exceptions import ValidationError
 
 
 def check_user(user):
     if user.is_authenticated:
         return user.first_name + ' ' + user.last_name
-    else:
-        return 'Anonymous'
+    return ''
 
 
 def positions(req):
@@ -45,6 +45,8 @@ def form_positions(req):
 
 def add_position(req):
     if req.POST:
+        if not req.POST['name']:
+            return render(req, './add_position.html', {'errors': ['empty']})
         new_pos = Position()
         new_pos.title = req.POST['name']
 
@@ -56,6 +58,12 @@ def add_position(req):
 def add_department(req):
     if req.POST:
         new_dep = Department()
+        if not (
+                req.POST['name'] and
+                req.POST['city'] and
+                req.POST['phone'] and
+                req.POST['address']):
+            return render(req, './add_department.html', {'errors': ['empty']})
         new_dep.title = req.POST['name']
         new_dep.city = req.POST['city']
         new_dep.phone = req.POST['phone']
@@ -68,16 +76,39 @@ def add_department(req):
 
 def add_employee(req):
     if req.POST:
-        print(req.POST)
         new_emp = Employee()
-        new_emp.first_name = req.POST['firstname']
-        new_emp.last_name = req.POST['lastname']
-        new_emp.date_birth = req.POST['birth']
-        new_emp.date_employment = req.POST['employed']
-        new_emp.phone = req.POST['phone']
-        new_emp.position = Position.objects.get(id__exact = int(req.POST['postId']))
-        new_emp.department = Department.objects.get(id__exact = int(req.POST['departmentId']))
-
-        new_emp.save()
+        name = check_user(req.user)
+        depts = Department.objects.all()
+        posits = Position.objects.all()
+        errors = []
+        try:
+            error = False
+            if not (req.POST['firstname'] and
+                    req.POST['lastname'] and
+                    req.POST['birth'] and
+                    req.POST['employed'] and
+                    req.POST['phone']):
+                errors.append('empty')
+                error = True
+            if req.POST['postId'] == 'Выберите должность...' and req.POST['departmentId'] == 'Выберите отдел...':
+                errors.append('models')
+                error = True
+            if error:
+                print('error')
+                raise Exception()
+            new_emp.first_name = req.POST['firstname']
+            new_emp.last_name = req.POST['lastname']
+            new_emp.date_birth = req.POST['birth']
+            new_emp.date_employment = req.POST['employed']
+            new_emp.phone = req.POST['phone']
+            new_emp.position = Position.objects.get(id__exact = int(req.POST['postId']))
+            new_emp.department = Department.objects.get(id__exact = int(req.POST['departmentId']))
+            new_emp.save()
+        except Exception as e:
+            if e.__class__ == ValidationError:
+                errors.append('dates')
+                print(errors)
+            return render(req, './add_employee.html', {'errors': errors,
+                                                       'departments': depts, 'positions': posits, 'name': name})
 
     return redirect("/employees")
